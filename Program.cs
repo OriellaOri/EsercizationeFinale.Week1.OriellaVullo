bd21eefd1a791d7cbc8e1eb699a7c4c90b4d9c5b
Oriella Vullo <oriella.vullo@avanade.com>
2021-07-09 10:58:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EsercizationeFinale.Week1.OriellaVullo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
